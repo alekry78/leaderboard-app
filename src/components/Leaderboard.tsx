3f7ccdeb06062dbed3d7ed2f5b1bb567437da8c5
aleksanderkrys <aleksander.krys@lightcode.com.pl>
2025-01-14 15:21:21 +0100

    sorting handled, added react-hook-form package

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -8,7 +8,7 @@ interface PlayerProps{
 
 const Leaderboard:React.FC = () => {
     const [players, setPlayers] = useState<PlayerProps[]>([]);
-
+    const [sort, setSort] = useState<string>('');
     useEffect(() => {
         fetch('/mockPlayers.json')
             .then(response => response.json())
@@ -16,6 +16,27 @@ const Leaderboard:React.FC = () => {
                 setPlayers(data)
             });
     }, []);
+    const handleHowToSort = () => {
+        switch(sort){
+            case '':
+                setSort('descending');
+                break;
+            case 'descending':
+                setSort('ascending');
+                break;
+            case 'ascending':
+                setSort('descending');
+        }
+    }
+    const handleSort = (a: PlayerProps, b: PlayerProps) => {
+        if(sort === 'descending'){
+            return b.score - a.score;
+        }
+        else if(sort === 'ascending'){
+            return a.score - b.score;
+        }
+        return 0;
+    }
     return (
         <main className='w-full flex flex-col justify-start items-center'>
             <section className='container mx-auto py-6'>
@@ -26,7 +47,7 @@ const Leaderboard:React.FC = () => {
                         <h3 className='w-1/3'>Player</h3>
                         <h3 className='w-1/3'>Score</h3>
                     </header>
-                    {players.sort((a, b) => b.score - a.score).map((player, index) => (
+                    {players.sort(handleSort).map((player, index) => (
                         <div className='w-full flex justify-between items-center p-4 border-b-2 last:border-0' key={`${index}${player.id}`}>
                             <span className='w-1/3'>{player.id}</span>
                             <span className='w-1/3'>{player.name}</span>
@@ -35,6 +56,9 @@ const Leaderboard:React.FC = () => {
                     ))}
                 </div>
             </section>
+            <button onClick={handleHowToSort} className='bg-blue-500 rounded-lg px-4 py-2 text-white'>
+                Sort by score {sort === 'descending' ? 'v' : sort === 'ascending' ? '^' : ''}
+            </button>
             <section className="container">
 
             </section>
